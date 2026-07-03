7f699cb89ca191f0bd99d574a00042f4a6ffb9f2
Sharath Vishwanath <sharathvishwanath46@gmail.com>
2026-07-04 01:24:35 +0530

    Test pre commit hook

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+// Add test comment to check pre-commit hook
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     tsconfigPaths: true,
   },
-})
+});
